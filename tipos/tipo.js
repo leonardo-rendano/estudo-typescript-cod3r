d65750f871e2d5bf96b6e0add8b4ae6a997e4f97
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-03-24 13:35:33 -0300

    first commit

---
+++ tipos/tipo.js
@@ -0,0 +1,58 @@
+"use strict";
+// string
+let nome = "João";
+//console.log(nome)
+// numbers
+let idade = 31;
+//console.log(idade)
+// boolean
+let possuiHobbies = false;
+//console.log(possuiHobbies)
+// tipos explícitos
+let minhaIdade;
+minhaIdade = 31;
+//console.log(typeof minhaIdade)
+// minhaIdade = '31'
+// array
+let hobbies = ['Treinar', 'Tocar', 'Correr'];
+// console.log(hobbies)
+// console.log(typeof hobbies)
+// tupla - Um array com uma quantidade pré-definida de tipos. 
+// Ex: Um array de três posições, com duas strings e um number
+let endereco = ['Av. Principal', 'RJ', 123];
+// enum - Estrutura onde são definidos valores pré-definidos
+// Ex: Segunda, terça, quarta, ... ou masculino, feminino
+var Cor;
+(function (Cor) {
+    Cor[Cor["Cinza"] = 0] = "Cinza";
+    Cor[Cor["verde"] = 1] = "verde";
+    Cor[Cor["Azul"] = 2] = "Azul";
+})(Cor || (Cor = {}));
+let minhaCor = Cor.Azul;
+console.log(minhaCor);
+// funções
+function retornaMeuNome() {
+    return nome;
+}
+console.log(retornaMeuNome());
+function digaOi() {
+    console.log('Oi');
+}
+digaOi();
+function multiplicar(numA, numB) {
+    return numA * numB;
+}
+// A tipagem que vem após os () é referente a tipagem do retorno
+// Ex: a função recebe dois parâmetros do tipo number e tem um retorno do tipo boolean
+const teste = function (a, b) {
+    return false;
+};
+// tipo função
+let calculo;
+calculo = multiplicar;
+console.log(calculo(3, 4));
+// objetos
+let usuario = {
+    nome: "João",
+    idade: 31
+};
---
+++ tipos/tipo.ts
@@ -0,0 +1,71 @@
+// string
+let nome: string = "João"
+//console.log(nome)
+
+// numbers
+let idade: number = 31
+//console.log(idade)
+
+// boolean
+let possuiHobbies: boolean = false
+//console.log(possuiHobbies)
+
+// tipos explícitos
+let minhaIdade: number
+minhaIdade = 31
+//console.log(typeof minhaIdade)
+// minhaIdade = '31'
+
+// array
+let hobbies: string[] = ['Treinar', 'Tocar', 'Correr']
+// console.log(hobbies)
+// console.log(typeof hobbies)
+
+// tupla - Um array com uma quantidade pré-definida de tipos. 
+// Ex: Um array de três posições, com duas strings e um number
+let endereco: [string, string, number] = ['Av. Principal', 'RJ', 123]
+
+// enum - Estrutura onde são definidos valores pré-definidos
+// Ex: Segunda, terça, quarta, ... ou masculino, feminino
+enum Cor {
+  Cinza, 
+  verde, 
+  Azul
+}
+
+let minhaCor: Cor = Cor.Azul
+console.log(minhaCor)
+
+// funções
+function retornaMeuNome(): string {
+  return nome 
+}
+
+console.log(retornaMeuNome())
+
+function digaOi(): void {
+  console.log('Oi')
+}
+
+digaOi()
+
+function multiplicar(numA: number, numB: number): number {
+  return numA * numB
+}
+
+// A tipagem que vem após os () é referente a tipagem do retorno
+// Ex: a função recebe dois parâmetros do tipo number e tem um retorno do tipo boolean
+const teste = function (a: number, b: number): boolean {
+  return false
+}
+
+// tipo função
+let calculo: (numA: number, numB: number) => number
+calculo = multiplicar
+console.log(calculo(3, 4))
+
+// objetos
+let usuario: { nome: string, idade: number } = {
+  nome: "João",
+  idade: 31
+}
