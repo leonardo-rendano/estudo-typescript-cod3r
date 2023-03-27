ff1da7c0f2c471e04e74f32fdc3d5ed556f3addb
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-03-27 16:56:54 -0300

    rest params e desctructurings

--- tipos/ecmascript/ecmascript.ts
+++ tipos/ecmascript/ecmascript.ts
@@ -21,9 +21,38 @@ const turmaB: string[] = ['João', 'Ricardo', 'Manoel', ...turmaA]
 console.log(turmaB)
 
 // Rest 
+// Caso seja necessário passar mais parâmetros além do args, os mesmos deverão ser passados
+// depois do operador rest
 const retornaArray = (...args: number[]): number[] => {
   return args
 }
 
 const numeros = retornaArray(1, 2, 3, 4, 5)
 
+// Rest e Spread (Tupla)
+const tupla: [number, string, boolean] = [1, 'abc', false]
+
+function tuplaParam1(a: number, b: string, c: boolean): void {
+  console.log(`1) ${a} ${b} ${c}`)
+} 
+
+tuplaParam1(...tupla)
+
+function tuplaParam2(...params: [number, string, boolean]) {
+  console.log(`2) ${params[0]}, ${params[1]} ${params[2]}`)
+}
+
+// Destructuring (array)
+const caracteristicas = ['Motor Zetec 1.8', 2020]
+// const motor = caracteristicas[0]
+// const ano = caracteristicas[1]
+
+const [motor, ano] = caracteristicas
+
+// Destructuring (objeto)
+const item = {
+  modelo: "SSD 480GB",
+  preco: 300
+}
+
+const { modelo, preco } = item
